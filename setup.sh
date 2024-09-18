bff87815519faeb31bf13c0d898e494c4e91f82d
OneLargeWater <raghavvr@gmail.com>
2024-09-18 18:30:48 -0500

    fixes

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 set -e
 
-sudo mkdir /mydata/Oasis
+sudo mkdir /mydata/Oasis -p
 sudo chown $USER /mydata/Oasis
 
-cd mydata/Oasis
+cd /mydata/Oasis
 git clone git@github.com:raghav-rangan/Oasis.git
 
 mkdir /mydata/Oasis/miniconda
@@ -12,7 +12,11 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x Miniconda3-latest-Linux-x86_64.sh
 ./Miniconda3-latest-Linux-x86_64.sh
 
+source install/etc/profile.d/conda.sh
+source ~/.bashrc
 conda create --name oasis python=3.9
+conda init
+sleep 1
 conda activate oasis
 
 conda install -c nvidia cuda cudnn
